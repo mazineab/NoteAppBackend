57942d177d3c3d43af14e725c35f7b068b6f250a
mazineab <mazinabjlal@gmail.com>
2024-07-12 15:47:14 +0000

    create_candidat

--- NotesApp/app/Models/Category.php
+++ NotesApp/app/Models/Category.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable=[
-        "nameCat"
-    ];
+    // protected $fillable=[
+    //     "nameCat",
+    //     "user_id"
+    // ];
+    
+    protected $guarded=['id'];
 }
